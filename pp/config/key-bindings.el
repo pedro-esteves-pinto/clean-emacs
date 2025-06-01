91f3dce922c940e70b97f9ecc06b5af257888638
Pedro Pinto <12345678+pedro-esteves-pinto@users.noreply.github.com>
2025-06-01 17:25:12 -0400

    compilation and magit keybindings

--- pp/config/key-bindings.el
+++ pp/config/key-bindings.el
@@ -1,8 +1,12 @@
 
 (global-set-key (kbd "M-x") 'counsel-M-x)
+
 (evil-leader/set-leader "<SPC>")
-(evil-leader/set-key "<SPC>" 'consult-projectile)
-(evil-leader/set-key "mm" 'magit-status "mb" 'vc-annotate) 
+(evil-leader/set-key
+  "<SPC>" 'consult-projectile
+  "b" 'projectile-compile-project
+  "m" 'magit-status)
+
 (global-set-key (read-kbd-macro "M-SPC") 'dabbrev-expand)
 (global-set-key (kbd "<f12>") 'consult-projectile)
 
